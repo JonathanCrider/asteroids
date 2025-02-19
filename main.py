861fb21475707674127f026cf9973a1941e98a0b
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2025-02-18 19:34:23 -0500

    adds player and draws to screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
   pygame.init()
@@ -18,6 +19,11 @@ def main():
       if event.type == pygame.QUIT:
           return
     screen.fill("black")
+
+    # Draw Player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT/ 2)
+    player.draw(screen)
+
     pygame.display.flip()
 
     dt = timeclock.tick(60) / 1000
