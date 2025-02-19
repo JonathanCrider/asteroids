13205c00afa38abaebd0139323526cf59e51243f
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2025-02-18 19:07:10 -0500

    adds timeclock

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ def main():
   print(f"Screen width: {SCREEN_WIDTH}")
   print(f"Screen height: {SCREEN_HEIGHT}")
 
+  # Setup
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  
+  timeclock = pygame.time.Clock()
+  dt = 0
+
   # Game Loop
   while True:
     for event in pygame.event.get():
@@ -16,6 +19,8 @@ def main():
           return
     screen.fill("black")
     pygame.display.flip()
+
+    dt = timeclock.tick(60) / 1000
 
 
 if __name__ == "__main__":
