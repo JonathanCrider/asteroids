f49ba477a92bb98b306ecfb19e8f9b1715c52068
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2025-02-20 08:52:32 -0500

    adds score mechanic and display

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def main():
 
   # Initialize Entities
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT/ 2)
+  # ship = pygame.image.load("assets/ship.webp")
   AsteroidField()
 
   # Game Loop
@@ -51,13 +52,15 @@ def main():
         if asteroid.collision(shot):
           asteroid.split()
           shot.kill()
+          player.score += 1
           break
       else:
         if asteroid.collision(player):
-          game_over(screen)
+          game_over(screen, player.score)
     for asset in drawable:
       asset.draw(screen)
 
+    score_display(screen, player.score)
     pygame.display.flip()
 
     dt = timeclock.tick(60) / 1000
@@ -66,18 +69,30 @@ def main():
 # HELPER FUNCTIONS
 
 
-def make_font(text):
-  font = pygame.freetype.Font(None, size=100)
+def make_font(text, type = "default"):
+  font_size = 100; div_sh = 2; div_sw = 2; div_tw = 2; div_th = 2
+  if type == "score":
+    div_sw = 1.15; div_sh = 10; font_size = 30; div_tw = 1
+
+  font = pygame.freetype.Font(None, size=font_size)
   text_rect = font.get_rect(text)
-  text_x = SCREEN_WIDTH/2 - text_rect.width/2
-  text_y = SCREEN_HEIGHT/2 - text_rect.height/2
+  text_x = SCREEN_WIDTH//div_sw - text_rect.width//div_tw
+  text_y = SCREEN_HEIGHT//div_sh - text_rect.height//div_th
   return font, (text_x, text_y)
 
 
-def game_over(screen):
-  text = "Game over!"
-  font, coords = make_font(text)
-  font.render_to(screen, coords, text, fgcolor="white", bgcolor="black")
+def score_display(screen, score):
+  text_score = f"Score: {score}"
+  font_score, s_coords = make_font(text_score, "score")
+  font_score.render_to(screen, s_coords, text_score, fgcolor="yellow", bgcolor="black")
+
+
+def game_over(screen, score):
+  text_gameover = f"Game over!"
+  font_gameover, coords = make_font(text_gameover)
+  font_gameover.render_to(screen, coords, text_gameover, fgcolor="white", bgcolor="black")
+  score_display(screen, score)
+  
   pygame.display.flip()
   pygame.time.wait(3000)
   sys.exit()
